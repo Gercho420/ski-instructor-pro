832422f0fed6e3eb255e33ef0d87d617f32f8a1f
Gercho420 <87453151+Gercho420@users.noreply.github.com>
2026-08-05 11:49:31 -0300

    Add NOT_ADMIN_ERR_MSG constant for access control

--- shared/const.ts
+++ shared/const.ts
@@ -1,3 +1,4 @@
 export const COOKIE_NAME = "session";
 export const ONE_YEAR_MS = 365 * 24 * 60 * 60 * 1000;
 export const UNAUTHED_ERR_MSG = "You must be logged in to access this resource.";
+export const NOT_ADMIN_ERR_MSG = "You must be an admin to access this resource.";
