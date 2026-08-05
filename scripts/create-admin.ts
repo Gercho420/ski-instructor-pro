e33d89276faeefdc1225865e26b6b7b697010839
Gercho420 <87453151+Gercho420@users.noreply.github.com>
2026-08-05 14:47:19 -0300

    Update create-admin.ts

--- scripts/create-admin.ts
+++ scripts/create-admin.ts
@@ -1,5 +1,5 @@
 import { db } from "../server/db.js";
-import { users } from "@shared/schema";
+import { users } from "../shared/schema.js"; // <-- Cambiado a ruta relativa
 import { eq } from "drizzle-orm";
 import bcrypt from "bcrypt";
 
